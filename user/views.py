52c4b3d6d2688894fe8561d0cfdf0bc96215abc8
OGUZ-GULESCI <oguz_gulesci@outlook.com>
2022-06-06 22:29:11 +0300

    blog

---
+++ user/views.py
@@ -0,0 +1,45 @@
+from django.shortcuts import render,redirect
+from . forms import RegisterForm,LoginForm
+from django.contrib.auth.models import User
+from django.contrib.auth import login,authenticate,logout
+from django.contrib import messages
+
+# Create your views here.
+def register(request):
+
+    form = RegisterForm(request.POST or None)
+    if form.is_valid():
+        username = form.cleaned_data.get("username")
+        password = form.cleaned_data.get("password")
+
+        newUser = User(username = username)
+        newUser.set_password(password)
+
+        newUser.save()
+        login(request,newUser)
+        messages.info(request, "Kayıt işleminiz başarıyla tamamlandı")
+        return redirect("index")
+    context = {"form" : form}
+    return render(request,"register.html",context)  
+        
+def loginUser(request):
+    form = LoginForm(request.POST or None)
+    context = {
+            "form" : form
+    }
+    if form.is_valid():
+        username = form.cleaned_data.get("username")
+        password = form.cleaned_data.get("password")
+        user = authenticate(username = username,password = password)
+        if user is None : 
+            messages.info(request,"kullanıcı Adı ve şifre hatalı tekrar deneyiniz")
+            return render(request,"login.html",context)
+        messages.success(request,"Giriş başarıyla gerçekleşti")
+        login(request,user)
+        return redirect("index")   
+    return render(request,"login.html",context)
+
+def logoutUser(request):
+    logout(request)
+    messages.success(request,"Çıkış yaptınız")
+    return redirect("index")
